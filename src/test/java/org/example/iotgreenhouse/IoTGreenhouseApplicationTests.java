22d2a037054c6cd2edbfeea0697c66c26de360ed
monster-programming <artemnovaev9@gmail.com>
2026-05-17 21:50:44 +0300

    Initial commit

---
+++ src/test/java/org/example/iotgreenhouse/IoTGreenhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.iotgreenhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTGreenhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
